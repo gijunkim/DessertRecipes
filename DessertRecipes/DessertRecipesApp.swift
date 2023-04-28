5c4c28f33dabf1c53c86eb65e20ce4ffa85ce3fa
gijunkim <gk392@cornell.edu>
2023-04-28 16:13:07 -0400

    Initial Commit

---
+++ DessertRecipes/DessertRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertRecipesApp.swift
+//  DessertRecipes
+//
+//  Created by June Kim on 4/28/23.
+//
+
+import SwiftUI
+
+@main
+struct DessertRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
